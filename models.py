eafbdfadd26d60fe83d1b363699eb1885485137d
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-24 02:38:35 +0000

    Created new tables for the database.

--- models.py
+++ models.py
@@ -1,17 +1,56 @@
 from app import db
 
-class Movie(db.Model):
-    __tablename__ = 'movies'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False, unique=True)
-    startYear = db.Column(db.Integer, nullable=False)
-    endYear = db.Column(db.Integer, nullable=True)
-
-    def __init__(self, title, startYear, endYear):
-        self.title = title
-        self.password = startYear
-        self.endYear = endYear
 
 def init_db():
     db.drop_all()
     db.create_all()
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    startYear = db.Column(db.Integer, nullable=False)
+    runtime = db.Column(db.Integer)
+
+    def __init__(self, title, startYear, runtime):
+        self.title = title
+        self.startYear = startYear
+        self.runtime = runtime
+
+
+class TVShow(db.Model):
+    __tablename__ = 'tvshows'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    startYear = db.Column(db.Integer, nullable=False)
+    endYear = db.Column(db.Integer, nullable=True)
+    current_season = db.Column(db.Integer)
+    seasons = db.Column(db.Integer)
+    current_episode = db.Column(db.Integer)
+    episodes = db.Column(db.Integer)
+
+    def __init__(self, title, startYear, endYear, seasons, episodes):
+        self.title = title
+        self.startYear = startYear
+        self.endYear = endYear
+        self.seasons = seasons
+        self.episodes = episodes
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+
+movieGenres = db.Table('movieGenres',
+
+                       db.Column('genre_id', db.ForeignKey(Genre.id), primary_key=True),
+                       db.Column('movie_id', db.ForeignKey(Movie.id), primary_key=True)
+                       )
+
+tvShowsGenres = db.Table('tvShowGenres',
+                         db.Column('genre_id', db.ForeignKey(Genre.id), primary_key=True),
+                         db.Column('tvshow_id', db.ForeignKey(TVShow.id), primary_key=True)
+                         )

a182f8ee73b27ae8c4be6ad1197ef55ee559630d
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-24 02:39:33 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask
+# IMPORTS
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+# CONFIG
 app = Flask(__name__)
-app.config['Secret_KEY'] = 'LongAndRandomSecretKey'
+app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+@app.route('/')
+def index():  # put application's code here
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()

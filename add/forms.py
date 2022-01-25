b5a26a87b774fb8a84a9b4c8a5a9fb78f9f84f4f
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-25 20:37:58 +0000

    Added ability to add new movie.

---
+++ add/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NewMovieForm(FlaskForm):
+    title = StringField(validators=[DataRequired()])
+    startYear = IntegerField()
+    runtime = IntegerField()
+    watchStatus = SelectField(u'Status', choices=['Plan to watch', 'Watching', 'Completed', 'Abandoned'], default="Plan to watch")
+    ageRestriction = SelectField(u'Restrictions', choices=['G', 'PG-13', 'R', 'NC-17'])
+    rating = IntegerField()
+    submit = SubmitField()

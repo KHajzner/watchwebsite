2944839024245077b1e80bc40f3e5612fa0abb84
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-26 03:17:07 +0000

    Moved new movie form and created a separate page for it.

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 
+from app import db
 from models import Movie
+from movies.forms import NewMovieForm
 
 movies_blueprint = Blueprint("movies", __name__, template_folder="templates")
 
@@ -14,3 +16,26 @@ def movies():
 
     return render_template('movies.html', watching=watching, toWatch=toWatch, completed=completed, abandoned=abandoned)
 
+
+@movies_blueprint.route('/new_movie', methods=['GET', 'POST'])
+def add_movie():
+    newMovie = NewMovieForm()
+    if newMovie.validate_on_submit():
+        movie = Movie.query.filter_by(title=newMovie.title.data).first()
+
+        if movie:
+            flash('Movie already exists')
+            return render_template("newMovie.html", newMovie=newMovie, form=newMovie)
+
+        new_movie = Movie(title=newMovie.title.data,
+                          startYear=newMovie.startYear.data,
+                          runtime=newMovie.startYear.data,
+                          watchStatus=newMovie.watchStatus.data,
+                          ageRestriction=newMovie.ageRestriction.data,
+                          rating=newMovie.rating.data
+                          )
+        db.session.add(new_movie)
+        db.session.commit()
+
+        return redirect(url_for("movies.movies"))
+    return render_template('newMovie.html', form=newMovie)

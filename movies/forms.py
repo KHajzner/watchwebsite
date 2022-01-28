7bb9b938f10f64ed0b195a259534b0973a761dfe
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-28 13:56:23 +0000

    Updated movie edit form and moved it to a separate page.

--- movies/forms.py
+++ movies/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, SubmitField
+from wtforms import StringField, IntegerField, SelectField, SubmitField, SelectMultipleField
 from wtforms.validators import DataRequired
+
+from models import Genre
+
+genres = Genre.query.all()
 
 
 class NewMovieForm(FlaskForm):
@@ -12,6 +16,13 @@ class NewMovieForm(FlaskForm):
     rating = IntegerField()
     submit = SubmitField()
 
+
 class EditMovie(FlaskForm):
     title = StringField(validators=[DataRequired()])
+    startYear = IntegerField()
+    runtime = IntegerField()
+    watchStatus = SelectField(u'Status', choices=['Plan to watch', 'Watching', 'Completed', 'Abandoned'], default="Plan to watch")
+    ageRestriction = SelectField(u'Restrictions', choices=['G', 'PG-13', 'R', 'NC-17'])
+    rating = IntegerField()
+    # genres = SelectMultipleField(u'Genre', choices=[genres])
     submit = SubmitField()
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,8 @@
+import copy
+
 from flask import Blueprint, render_template, flash, redirect, url_for
 
+import movies.forms
 from app import db
 from models import Movie
 from movies.forms import NewMovieForm, EditMovie
@@ -13,8 +16,34 @@ def movies():
     toWatch = Movie.query.filter_by(watchStatus="Plan to watch").all()
     completed = Movie.query.filter_by(watchStatus="Completed").all()
     abandoned = Movie.query.filter_by(watchStatus="Abandoned").all()
+    return render_template('movies.html', watching=watching, toWatch=toWatch, completed=completed,
+                       abandoned=abandoned)
+
+
+@movies_blueprint.route('/<int:id>/update', methods=['GET', 'POST'])
+def update_movie(id):
     form = EditMovie()
-    return render_template('movies.html', form=form, watching=watching, toWatch=toWatch, completed=completed, abandoned=abandoned)
+    movie = Movie.query.filter_by(id=id).first()
+    if form.validate_on_submit():
+        Movie.query.filter_by(id=id).update({"title": form.title.data})
+        Movie.query.filter_by(id=id).update({"startYear": form.startYear.data})
+        Movie.query.filter_by(id=id).update({"runtime": form.runtime.data})
+        Movie.query.filter_by(id=id).update({"watchStatus": form.watchStatus.data})
+        Movie.query.filter_by(id=id).update({"ageRestriction": form.ageRestriction.data})
+        Movie.query.filter_by(id=id).update({"rating": form.rating.data})
+        # Movie.query.fitter_by(id=id).update({"genre": form.genres.data})
+
+        db.session.commit()
+        return movies()
+    movie_copy = copy.deepcopy(movie)
+
+    form.title.data = movie_copy.title
+    form.startYear.data = movie_copy.startYear
+    form.runtime.data = movie_copy.runtime
+    form.watchStatus.data = movie_copy.watchStatus
+    form.ageRestriction.data = movie_copy.ageRestriction
+    form.rating.data = movie_copy.rating
+    return render_template('update_movie.html', form=form)
 
 
 @movies_blueprint.route('/new_movie', methods=['GET', 'POST'])

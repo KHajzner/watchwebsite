11622498d49a37072dcbdb0f0226c1aedbda4fa8
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-28 02:02:27 +0000

    Added form for deleting genres - doesn't fully work yet.

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ class NewGenre(FlaskForm):
     submit = SubmitField()
 
 
+class DeleteGenre(FlaskForm):
+    name2 = StringField(validators=[DataRequired()])
+    submit2 = SubmitField()
+
+
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html')
@@ -29,19 +34,27 @@ def index():  # put application's code here
 def genres():
     from models import Genre
     form = NewGenre()
+
+    form2 = DeleteGenre()
     genres = Genre.query.all()
-    if form.validate_on_submit():
+    if form.submit.data and form.validate():
         genre = Genre.query.filter_by(name=form.name.data).first()
         if not genre:
             genre = Genre(name=form.name.data)
             db.session.add(genre)
             db.session.commit()
-        return redirect(url_for('genres', genres=genres, form=form))
-    return render_template('genres.html', genres=genres, form=form)
+        return redirect(url_for('genres', genres=genres, form=form, form2=form2))
+
+    # TODO: genre deletion doesn't commit to the database
+    if form2.submit2.data and form2.validate():
+        Genre.query.filter_by(name=form2.name2.data).delete()
+        db.session.commit()
+        return redirect(url_for('genres', genres=genres, form=form, form2=form2))
+
+    return render_template('genres.html', genres=genres, form=form, form2=form2)
 
 
 if __name__ == '__main__':
-
     from tvshows.views import tvshow_blueprint
     from movies.views import movies_blueprint
 

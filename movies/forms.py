d2f7c66effef379586efa92df6c5372e5673bdf2
Kalina <k.hajzner1@ncl.ac.uk>
2022-01-30 00:30:22 +0000

    Temporarily removed genres.

--- movies/forms.py
+++ movies/forms.py
@@ -2,11 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, SubmitField, SelectMultipleField
 from wtforms.validators import DataRequired
 
-from models import Genre
-
-genres = Genre.query.all()
-
-
 class NewMovieForm(FlaskForm):
     title = StringField(validators=[DataRequired()])
     startYear = IntegerField()
@@ -24,5 +19,5 @@ class EditMovie(FlaskForm):
     watchStatus = SelectField(u'Status', choices=['Plan to watch', 'Watching', 'Completed', 'Abandoned'], default="Plan to watch")
     ageRestriction = SelectField(u'Restrictions', choices=['G', 'PG-13', 'R', 'NC-17'])
     rating = IntegerField()
-    # genres = SelectMultipleField(u'Genre', choices=[genres])
+    # genres = SelectMultipleField(u'Genre', coerce=int)
     submit = SubmitField()
